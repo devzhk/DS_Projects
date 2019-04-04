1d0a014f826cd0267774e3815f63093396d927e4
devzhk <devzhk@sjtu.edu.cn>
2019-04-04 16:57:53 +0800

    Update Dataset.py

--- Project1/Dataset.py
+++ Project1/Dataset.py
@@ -1,16 +1,15 @@
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-
-feature_path = 'RawData/AwA2-features.txt'
+import torch.utils.data as data
+feature_path = 'RawData/features.npy'
 label_path = 'RawData/AwA2-labels.txt'
 pca_path = 'Data/pca.npy'
 tsne_path = 'Data/tsne3.npy'
-
+vae128_path = 'Data/VAE-128.npy'
 
 def dataset(data_type):
     labels = np.loadtxt(label_path, dtype=int)
-    # std_features = 0
     if data_type == 'raw':
         features = np.loadtxt(feature_path, delimiter=' ')
         scaler = StandardScaler()
@@ -21,6 +20,8 @@ def dataset(data_type):
         print('Load PCA features')
     elif data_type == 'tsne':
         std_features = np.load(tsne_path)
+    elif data_type == 'VAE-128':
+        std_features = np.load(vae128_path)
     else:
         print('No such data type')
     print(labels.shape, std_features.shape)
@@ -39,6 +40,18 @@ def make_idx():
     np.save('Data/train_idx.npy', train_idx)
     np.save('Data/test_idx.npy', test_idx)
 
+
+class AwA2(data.Dataset):
+    def __init__(self):
+        self.features = np.load(feature_path)
+        print(self.features.shape)
+        self.labels = np.loadtxt(label_path, dtype=int)
+
+    def __getitem__(self, item):
+        return self.features[item, :], self.labels[item]
+
+    def __len__(self):
+        return self.labels.shape[0]
 
 if __name__ == '__main__':
     make_idx()
