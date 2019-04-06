9eafde3bbd02803008f4605a7fcc7e8293641096
devzhk <devzhk@sjtu.edu.cn>
2019-04-06 12:49:08 +0800

    Update train.py

--- Project1/train.py
+++ Project1/train.py
@@ -9,18 +9,21 @@ from torch import nn
 from Dataset import AwA2
 from VAE import VAE
 from VAE import reparametrize
+from tensorboardX import SummaryWriter
 # import Dataset
 from sklearn.preprocessing import StandardScaler
 
 feature_path = 'RawData/AwA2-features.txt'
 label_path = 'RawData/AwA2-labels.txt'
-save_path = 'models/VAE-'
+beta = 0.1
+save_path = 'models/v-b%.1fVAE-' % beta
 
-epoch_num = 150
+epoch_num = 300
 batch_size = 64
-z_dim = 128
+z_dim = 64
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-mode = 'eval'
+mode = 'train'
+
 
 def kl_loss(mu, logvar):
     kld_element = -0.5 * (1 + logvar - mu.pow(2) - logvar.exp())
@@ -50,16 +53,16 @@ def save_z(path):
         z = z.detach().cpu().numpy()
         z_list = np.append(z_list, z, axis=0)
         print(z_list.shape)
-    np.save('Data/VAE-%d.npy' % z_dim, z_list)
-
+    np.save('Data/b%.1fVAE-%d.npy' % (beta, z_dim), z_list)
 
 
 if __name__ == '__main__':
     # labels = np.loadtxt(label_path, dtype=int)
     # features = np.loadtxt(feature_path, delimiter=' ')
     if mode == 'eval':
-        save_z('models/VAE-128.pkl')
+        save_z(save_path + '%d.pkl' % z_dim)
         exit()
+    writer = SummaryWriter()
     data = AwA2()
     loader = DataLoader(dataset=data, batch_size=batch_size, shuffle=True)
     net = VAE(z_dim=z_dim)
@@ -76,13 +79,15 @@ if __name__ == '__main__':
             f = f.to(device)
             f = f.float()
             recon_f, mu, logvar = net(f)
-            loss = (recon_loss(f, recon_f) + kl_loss(mu, logvar)) / f.shape[0]
+            loss = (recon_loss(f, recon_f) + beta * kl_loss(mu, logvar)) / f.shape[0]
             optim.zero_grad()
             loss.backward()
             optim.step()
             train_loss += loss.item()
         print('epoch%d, Loss%.4f' % (e, train_loss / len(loader)))
+        writer.add_scalar('b0.1VAE_Loss', train_loss / len(loader), e)
     torch.save(net.state_dict(), save_path + '%d.pkl' % z_dim)
+    writer.close()
 
 
 
